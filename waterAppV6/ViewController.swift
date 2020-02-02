53dd8656664f35de010869ce1bae9d12e8e200d4
Charlie Nguyen <charliehoangnguyen@gmail.com>
2020-02-02 09:28:03 -0800

    Initial Commit

---
+++ waterAppV6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  waterAppV6
+//
+//  Created by Charlie on 2/2/20.
+//  Copyright © 2020 Charlie Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
